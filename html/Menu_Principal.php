8e74cc7554c76429d980c38976be6b7985df16ed
Mitikx <T.SAN-JUAN@lprs.fr>
2024-02-09 10:06:16 +0100

    chor : main menue

---
+++ html/Menu_Principal.php
@@ -0,0 +1,18 @@
+<HTML>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<link rel="stylesheet" href="../css/les-styles.css">
+<link rel="stylesheet" href="../css/les-styles.css">
+<body>
+<class = "commande">
+Professeur : <select name="professeur" id="professeur-select">
+    <option value="">Choix Professeur</option>
+    <option value="M.Lestrelin">M.Lestrelin</option>
+    <option value="M.Lemoine">M.Lemoine</option>
+    <option value="M.Mattei">M.Mattei</option>
+Unite pedagogique : <input type="text">
+        <h2>Menue Principal</h2>
+        <button class="btn-orange" name="btncommande">Commander</button>
+        <button class="btn-rouge" name="btnajout">Ajouter</button>
+        <button class="btn-vert" name="btnerreur">Erreur</button>
+</body>
+</HTML>
