ae332fe64e2bfe2541883ce73d3c73255b42a099
Mitikx <T.SAN-JUAN@lprs.fr>
2024-02-10 15:57:53 +0100

    Feat : gestionDeProjet and demandeDePrix

--- html/Menu_Principal.php
+++ html/Menu_Principal.php
@@ -1,24 +1,24 @@
 <HTML>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
 <link rel="stylesheet" href="../css/les-styles.css">
-<link rel="stylesheet" href="../css/les-styles.css">
+<link rel="stylesheet" href="../css/mes-styles.css">
 <body style="background-color:white;">
 
-Professeur : <select name="professeur" id="professeur-select">
+Professeur : <select class="scroll"  name="professeur" id="professeur-select" required>
     <option value="">Choix Professeur</option>
     <option value="M.Lestrelin">M.Lestrelin</option>
     <option value="M.Lemoine">M.Lemoine</option>
     <option value="M.Mattei">M.Mattei</option>
 </select>
 <br>
-Unite pedagogique : <select name="unite-Pedagogique" id="unite-select">
+Unite pedagogique : <select class="scroll" name="unite-Pedagogique" id="unite-select" required>
     <option value="">Choix de classe</option>
     <option></option>
 </select>
 <br>
-Date : <input type="date" id="dateCommande" name="date">
+Date : <input type="date" id="dateCommande" name="date" required>
 <br>
-Fournisseur : <select name="fournisseur" id="fournisseur-select">
+Fournisseur : <select class="scroll" name="fournisseur" id="fournisseur-select" required>
     <option value="">Choix du fournisseur</option>
     <option></option>
 </select>
@@ -28,7 +28,7 @@ Fournisseur : <select name="fournisseur" id="fournisseur-select">
 <button class="btn-rouge" name="btnajout">Ajouter</button>
 <button class="btn-vert" name="btnerreur">Erreur</button>
 <form action="gestionDeProjet.php">
-<button type="submit" class="btn-bleu" name="btnprojet">Projet</button>
+<button type="submit" class="btn-bleu" name="btnprojet">Débit</button>
 </form>
 </body>
 </HTML>
