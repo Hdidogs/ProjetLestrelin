a08cd0573f451c8c4b8015c62594dc0b4abc4d6b
Mitikx <T.SAN-JUAN@lprs.fr>
2024-02-29 14:47:18 +0100

    Fix : envoie fichier and changement de nom

--- php/projet/projet.php
+++ php/projet/projet.php
@@ -3,19 +3,21 @@ include '../SQLConnexion.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupère les données du formulaire ou assigne null si elles n'existent pas
-    $nom = $_POST['nom'];
+
 
     if (isset($_FILES['image'])) {
         // Vérifie si le téléchargement du fichier a réussi
         if ($_FILES['image']['error'] == 0) {
             // Définit le répertoire de destination pour les fichiers téléchargés
-            $target_dir = 'uploads/';
+            $target_dir = "../../assets/images/";
             // Vérifie si le répertoire existe, sinon le crée
             if (!file_exists($target_dir)) {
                 mkdir($target_dir, 0777, true);
             }
+            $nouveauNom = $_POST['nouveauNom'];
+            $extension = pathinfo($_FILES['image']['name'],PATHINFO_EXTENSION);
             // Construit le chemin complet du fichier image
-            $imagePath = $target_dir . $_FILES['image']['name'];
+            $imagePath = $target_dir . $nouveauNom . ".". $extension;
             // Tente de déplacer le fichier téléchargé vers le répertoire de destination
             if (move_uploaded_file($_FILES['image']['tmp_name'], $imagePath)) {
                 // Si le déplacement réussit, affiche un message de succès
@@ -29,4 +31,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo "Erreur lors du téléchargement de l'image. Code d'erreur : " . $_FILES['image']['error'];
         }
     }
+    $conn = new SQLConnexion();
+    $stmt = $conn-> conbdd()->prepare("INSERT INTO projet (image) VALUES (?)");
+    $stmt->execute([$imagePath]);
+    echo 'Image ajouté avec succès!';
 }
