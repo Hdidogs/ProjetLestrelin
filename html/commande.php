1a6e7922388a27fd578f092102eb4d307b3091a4
Mitikx <T.SAN-JUAN@lprs.fr>
2024-03-08 10:05:23 +0100

    Fix : Commande and gestionProjet

--- html/commande.php
+++ html/commande.php
@@ -1,1 +1,77 @@
 <?php
+include "../php/SQLConnexion.php";
+$conn = new SQLConnexion();
+$req = $conn->conbdd()->query("SELECT * FROM fournisseur");
+$fournisseur = $req->fetchAll();
+$req = $conn->conbdd()->query("SELECT * FROM user WHERE ref_fonction = 1");
+$professeur = $req->fetchAll();
+$req = $conn->conbdd()->query("SELECT * FROM classe");
+$classe = $req->fetchAll();
+$req = $conn->conbdd()->query("SELECT * FROM matierecommande");
+$nposte = $req->fetchAll();
+?>
+<html>
+<body>
+<form action="../php/commande/commande.php" method="post" enctype="multipart/form-data">
+
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Fournisseur :</label>
+            <select name="fournisseur">
+                <option><?php foreach ($fournisseur as $liste): ?>
+                <option value="<?php echo $liste['id_fournisseur']; ?>"><?php echo $liste['mail']; ?></option>
+                <?php endforeach;?>
+            </select>
+        </div>
+
+
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Professeur :</label>
+            <select class="form-control" id="exampleFormControlSelect1" name="professeur">
+                <option><?php foreach ($professeur as $liste): ?>
+                <option value="<?php echo $liste['id_user']; ?>"><?php echo $liste['nom']; ?></option>
+                <?php endforeach;?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Unite Pedagogique :</label>
+            <select class="form-control" id="exampleFormControlSelect1" name="uniteP">
+                <option><?php foreach ($classe as $liste): ?>
+                <option value="<?php echo $liste['id_classe']; ?>"><?php echo $liste['libelle']; ?></option>
+                <?php endforeach;?>
+            </select>
+        </div>
+
+
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">N°Poste :</label>
+            <select class="js-example-basic-multiple" name="states[]" multiple="multiple" name="numeroP">
+                <option><?php foreach ($nposte as $liste): ?>
+                <option value="<?php echo $liste['id_matierecommande']; ?>"><?php echo $liste['prix']; ?><?php echo $liste['quantite']; ?></option>
+                <?php endforeach;?>
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">N°Devis :</label>
+            <input type="text" name="ndevis">
+        </div>
+
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1" >Condition de règlement :</label>
+            <br>
+            <textarea class="form-control" name="comment" id="exampleFormControlTextarea1" rows="3" cols="30" placeholder="Condition de règlement :"></textarea>
+        </div>
+
+
+        <div class="modal-footer">
+            <input type="hidden" name="submit">
+            <input type="submit" name="submit" value="Envoyer">
+        </div>
+</form>
+<script>
+    $(document).ready(function() {
+        $('.js-example-basic-multiple').select2();
+    });
+</script>
+</body>
+</html>
--- php/commande/commande.php
+++ php/commande/commande.php
@@ -1,1 +1,53 @@
 <?php
+use Aspose\Words\WordsApi;
+use Aspose\Words\Model\Requests\{ConvertDocumentRequest};
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
+$wordsApi = new WordsApi('####-####-####-####-####', '##################');
+
+$doc = "../../html/commande.php";
+$request = new ConvertDocumentRequest(
+    $doc, "pdf", NULL, NULL, NULL, NULL
+);
+$convert = $wordsApi->convertDocument($request);
+
+$mail = new PHPMailer(true);
+
+try {
+
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    $mail->Username = 'mrlestrelin0@gmail.com';                     //SMTP username
+    $mail->Password = 'Lestrelin0!';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom("mrlestrelin@gmail.com", 'Lestrelin');
+    $mail->addAddress($_POST['fournisseur']);     //Add a recipient
+    $mail->addAddress($_POST['mailDDFPT']);  //Name is optional
+    $mail->addAddress('mrlestrelin@gmail.com');
+    $mail->addReplyTo('mrlestrelin@gmail.com', 'Lestrelin');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $_POST['ndevis'];
+    $mail->Body = $_POST['comment'];
+    $mail->AltBody = $_POST['comment'];
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
