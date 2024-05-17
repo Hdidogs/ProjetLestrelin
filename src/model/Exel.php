13c14d4929c482841ff3966936684bf8937888e9
May <nospher4z@gmail.com>
2024-05-17 08:16:01 +0200

    Fix : commandeMatiere

--- src/model/Exel.php
+++ src/model/Exel.php
@@ -2,23 +2,24 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../vendor/phpoffice/phpspreadsheet/src/Bootstrap.php';
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
 class Exel
 {
-    public static function createExel($date,$num,$fournisseur,$nom,$prenom,$mail): void
+    public static function createExel($date, $num, $fournisseur, $nom, $prenom, $mail): void
     {
         $scriptPath = dirname(__FILE__);
         $exelFilePath = $scriptPath . '/../../assets/exel/Bon de Commande.xlsx';
         $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($exelFilePath);
         $sheet = $spreadsheet->getActiveSheet();
 
-        $sheet->setCellValue('B8' , $num);
-        $sheet->setCellValue('C4' , $date);
-        $sheet->setCellValue('B5' , $fournisseur);
-        $sheet->setCellValue('B6' , $nom);
-        $sheet->setCellValue('B6' , $prenom);
-        $sheet->setCellValue('B7' , $mail);
+        $sheet->setCellValue('B8', $num);
+        $sheet->setCellValue('C4', $date);
+        $sheet->setCellValue('B5', $fournisseur);
+        $sheet->setCellValue('B6', $nom);
+        $sheet->setCellValue('B6', $prenom);
+        $sheet->setCellValue('B7', $mail);
 
         $writer = new Xlsx($spreadsheet);
-        $writer->save($scriptPath . '/../../assets/exel/Bon de Commande' . " " .$date . '.xlsx');
+        $writer->save($scriptPath . '/../../assets/exel/Bon de Commande' . " " . $date . '.xlsx');
     }
 }
